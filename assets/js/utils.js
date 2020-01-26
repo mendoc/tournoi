d5b5517e4433faccf743314dd95b28f81b2c4e87
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-01-26 00:18:32 +0000

    cote flow - liste des joueurs participants

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -65,6 +65,7 @@ function loadFenetre(fen) {
                 write(`/tournoi/players/${playerID}`, player)
             });
         }
+        break;
         case "choice": {
             let playerKey = getKey();
             listen("/tournoi/players", function (data) {
@@ -77,8 +78,54 @@ function loadFenetre(fen) {
 
             $('#fen-choice select').change(function () {
                 let value = $(this).val();
+                let name = $('#fen-choice select option:selected').text();
                 if (value === "0") return;
-                write(`/tournoi/duel/players/${playerKey}`, { id: value })
+                write(`/tournoi/duel/players/${playerKey}`, { id: value, name: name })
+            });
+        }
+        break;
+        case "config": {
+            listen("/tournoi/config", function (config) {
+                if (config) {
+                    $('#fen-config [name="questions"]').val(config.source);
+                    $('#fen-config [name="feuille"]').val(config.sheet);
+                    $('#fen-config [name="temps"]').val(config.temps);
+                    $('#fen-config [name="quest-duel"]').val(config.quest_duel);
+                }
+            });
+
+            $('#fen-config [type="submit"]').click(function () {
+                let source = $('#fen-config [name="questions"]').val();
+                let sheet = $('#fen-config [name="feuille"]').val();
+                let temps = $('#fen-config [name="temps"]').val();
+                let quest_duel = $('#fen-config [name="quest-duel"]').val();
+                if (!source) return;
+                let config = {
+                    source: source,
+                    sheet: sheet,
+                    temps: temps,
+                    quest_duel: quest_duel
+                }
+                write('/tournoi/config/', config)
+            });
+
+            $('#fen-config [type="button"]').click(function () {
+                location.href = "?fen=classement"
+            });
+        }
+        break;
+        case "classement": {
+            listen("/tournoi/duel/players", function (players) {
+                if (players) {
+                    let n = 1;
+                    $('#fen-classement table tr:first').empty();
+                    $.each(players, function (index, player) {
+                        if (n > 4) return;
+                        cons(index)
+                        $('#fen-classement table tr:first').append(`<td>${player.name}</td>`);
+                        n++;
+                    });
+                }
             });
         }
     }
